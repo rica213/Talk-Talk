da2bdca7b1b7a1966b1b5868b388e46fb903fba3
rica213 <25f5k252l@mozmail.com>
2023-08-24 13:13:40 +0300

    Implement the server code

--- talk_talk.rb
+++ talk_talk.rb
@@ -8,4 +8,34 @@ class TalkTalk
     printf("Chatserver started on port %d\n", port)
     @descriptors << @server_socket
   end
+
+  def run
+    loop do
+      result = select(@descriptors, nil, nil, nil)
+      next if result.nil?
+      (result[0]).each do |socket|
+        if socket.eql?(@server_socket)
+          accept_new_connection
+        elsif socket.eof?
+          str = format('Client left %<socket.peeraddr[2]>s: %<socket.peeraddr[1]>s\n')
+          broadcast_string(str, socket)
+          socket.close
+          @descriptors.delete(socket)
+        else
+          str = format('[%<socket.peeraddr[2]>s|%<socket.peeraddr[1]>s]: %<socket.gets>s')
+          broadcast_string(str, socket)
+        end
+      end
+    end
+  end
+
+  private
+
+  def broadcast_string(str, omit_socket)
+    
+  end
+
+  def accept_new_connection
+
+  end
 end
